cebe25f85f6135a06f8d3f60667c8547ae23c644
drmeloy <drmeloy@gmail.com>
2020-02-03 18:19:38 -0700

    tested that both container components work with styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,6 @@ import CatFacts from '../containers/CatFacts.jsx';
 import CatFactsFn from '../containers/CatFactsFn.jsx';
 
 export default function App() {
-  return <CatFactsFn />;
+  return <CatFacts />;
 }
   
